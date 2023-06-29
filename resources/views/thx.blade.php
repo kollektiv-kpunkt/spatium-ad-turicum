317149308d8a6aad5c6d7f97ef470a88908686f0
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-06-29 15:02:01 +0200

    Change color of title on thx page

--- resources/views/thx.blade.php
+++ resources/views/thx.blade.php
@@ -3,7 +3,7 @@ $parser = new Illuminate\Support\Str;
 ?>
 <x-app-layout title="Danke für deine Unterschrift">
     <div class="sat-container sat-container__md">
-        <h1 class="text-2xl md:text-3xl lg:text-5xl text-center font-black uppercase text-red my-6">Danke für deine Unterschrift, {{$supporter->data["fname"]}}!</h1>
+        <h1 class="text-2xl md:text-3xl lg:text-5xl text-center font-black uppercase text-rot my-6">Danke für deine Unterschrift, {{$supporter->data["fname"]}}!</h1>
         <p class="text-lg"><b>Danke, dass du unseren Brief unterschrieben hast, {{$supporter->data["fname"]}}!</b> Zusammen können wir es schaffen, Druck auf den Stadtrat aufzubauen.</p>
         <p>Damit wir möglichst viele Menschen erreichen, sind wir auf deine Hilfe angewiesen! <b>Kannst du unseren Brief in deinem Umfeld teilen?</b> Danke für deine Hilfe!</p>
         <x-social-share/>
