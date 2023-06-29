d1d4082c08f7b3e28e4e0ff20af0295cb5617b3b
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-06-29 15:00:39 +0200

    Change color of form buttoChange color of form buttonn

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -8,7 +8,7 @@
                 @csrf
                 <input type="hidden" name="source" value="{{$_COOKIE['sat_source'] ?? ""}}">
                 <input type="hidden" name="locale" value="{{app()->getLocale()}}">
-                <button type="submit" class="sat-button !bg-white !text-green w-full flex justify-center items-center gap-x-2 mt-2 font-bold">{{__("sat.form.submit") }}  <i class="icofont-duotone icofont-apply"></i></button>
+                <button type="submit" class="sat-button !bg-white !text-gruen w-full flex justify-center items-center gap-x-2 mt-2 font-bold">{{__("sat.form.submit") }}  <i class="icofont-duotone icofont-apply"></i></button>
             </form>
         </div>
     </div>
